0bbda671a9587980f265b46a4d8a658b716cf7eb
jialiuBellus3d <jialiu@bellus3d.com>
2019-04-20 10:37:14 -0700

    added scroll view

--- videoeditor/src/main/java/com/itu/videoeditor/MainActivity.java
+++ videoeditor/src/main/java/com/itu/videoeditor/MainActivity.java
@@ -1,19 +1,7 @@
 package com.itu.videoeditor;
 
 import android.app.Activity;
-import android.content.ClipData;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends Activity {
 
